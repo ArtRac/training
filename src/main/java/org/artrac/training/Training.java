679af81756a8ef391a61db9675daa45b5903be9c
iglak <xeaglak@gmail.com>
2025-09-16 22:37:46 +0200

    zadanie 4 tu nawet nie ma co debugowac

--- src/main/java/org/artrac/training/Training.java
+++ src/main/java/org/artrac/training/Training.java
@@ -1,39 +1,29 @@
 package org.artrac.training;
 
-//zero ai, cale moje (wiem widac)
+//zrobilem caly kod na swiezo jak co
+//podejrzanie proste to cwiczenie tak szczerze, tu nawet nie ma co debugowac
 
 public class Training {
 
-    static int number = 1;
-
     public static void main(String[] args){
-        System.out.println("testowa liczba pierwsza:");
-        System.out.println(isPrime(7));
 
-        System.out.println("dziesiec kolejnych liczb pierwszych");
+        double number = 1.5d;
 
-        for (int i = 0; i < 10;){
-            if (isPrime(number)){
-                System.out.println(number);
-                i++;
-            }
-            number++;
+        //przykladowy output, nie debug
+        if(isPositive(number)){
+            System.out.println("liczba wynosi " + number + " i jest dodatnia");
+        } else {
+            System.out.println("liczba wynosi " + number + " i jest ujemna");
         }
 
     }
 
-    static boolean isPrime(int number) {
-        for(int i = 1; i <= number; i++){
-            if (number % i == 0 && i != number && i != 1){
-                return false;
-            } else if(number == 1) {
-                return false;
-            } else if (i == number && number / i == 1){
-                return true;
-            }
-        }
-        //fallback
-        return false;
+    public static boolean isPositive(double number){
+        return number>0;
+
+        /*specjalnie jest to napisane tak, zeby pytaniem bylo "czy jest dodatnie".
+        zrobilem tak poniewaz 1. pytanie jest o sprawdzenie czy jest dodatnie i 2. zero nie jest zadnym
+        wiec musialbym jako string returnowac zeby miec trzy odpowiedzi */
     }
 
 }
--- src/test/java/org/artrac/training/TrainingTest.java
+++ src/test/java/org/artrac/training/TrainingTest.java
@@ -7,17 +7,16 @@ import org.junit.jupiter.params.provider.ValueSource;
 class TrainingTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 1, 50000001})
-    void isPrimeFalseTest(int number){
-        boolean result = Training.isPrime(number);
-        Assertions.assertFalse(result);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {11})
-    void isPrimeTrueTest(int number){
-        boolean result = Training.isPrime(number);
+    @ValueSource(doubles = {1 , 1.5})
+    void isPositiveTest(double number){
+        boolean result = Training.isPositive(number);
         Assertions.assertTrue(result);
     }
 
+    @ParameterizedTest
+    @ValueSource(doubles = {-1, -0.5, 0})
+    void isFalseTest(double number){
+        boolean result = Training.isPositive(number);
+        Assertions.assertFalse(result);
+    }
 }
