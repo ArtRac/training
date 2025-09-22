296df1af917f0aab7b9f8b903ff6b7a29b2d38e0
iglak <xeaglak@gmail.com>
2025-09-22 13:31:06 +0200

    zadanie 5

--- src/main/java/org/artrac/training/Training.java
+++ src/main/java/org/artrac/training/Training.java
@@ -3,54 +3,76 @@ package org.artrac.training;
 //zrobilem caly kod na swiezo jak co
 //podejrzanie proste to cwiczenie tak szczerze, tu nawet nie ma co debugowac
 
+import java.util.Arrays;
+
 public class Training {
 
-    public static void main(String[] args){
+    public static int twoElements(int x, int y){
+        int[] array = new int[]{x, y};
+        return array[0] + array[1];
+    }
 
-        double number = 1.5d;
+    public static int biggestNumber(int x, int y, int z){
+        int[] numbers = new int[]{x, y, z};
 
-        //przykladowy output, nie debug
-        if(isPositive(number)){
-            System.out.println("liczba wynosi " + number + " i jest dodatnia");
-        } else {
-            System.out.println("liczba wynosi " + number + " i jest ujemna");
-        }
+        Arrays.sort(numbers);
+
+        return numbers[numbers.length-1];
 
     }
 
-    public static boolean isPositive(double number){
-        return number>0;
+    public void sudokuBoard(){
+        /*podana plansza: https://upload.wikimedia.org/wikipedia/commons/2/2d/Sudoku_przyklad.png
+        pierwszy numer - poziomo, drugi - pionowo
+        */
+        int[][] board = new int[9][9];
+        board[0][0] = 2;
+        board[3][0] = 6;
+        board[5][0] = 7;
+        board[6][0] = 5;
 
-        /*specjalnie jest to napisane tak, zeby pytaniem bylo "czy jest dodatnie".
-        zrobilem tak poniewaz 1. pytanie jest o sprawdzenie czy jest dodatnie i 2. zero nie jest zadnym
-        wiec musialbym jako string returnowac zeby miec trzy odpowiedzi */
-        System.out.println("testowa liczba pierwsza:");
-        System.out.println(isPrime(7));
+        board[7][1] = 9;
+        board[8][1] = 6;
 
-        System.out.println("dziesiec kolejnych liczb pierwszych");
+        board[0][2] = 6;
+        board[2][2] = 7;
+        board[5][2] = 1;
+        board[6][2] = 3;
 
-        for (int i = 0; i < 10;){
-            if (isPrime(number)){
-                System.out.println(number);
-                i++;
-            }
-            number++;
-        }
-
+        //i tak dalej, wiem o co chodzi a nie chce mi sie dalej wypelniac ¯\_(ツ)_/¯
     }
 
-    static boolean isPrime(int number) {
-        for(int i = 1; i <= number; i++){
-            if (number % i == 0 && i != number && i != 1){
-                return false;
-            } else if(number == 1) {
-                return false;
-            } else if (i == number && number / i == 1){
-                return true;
-            }
-        }
-        //fallback
-        return false;
+    public void chessBoard(){
+
+        /*stan partii: https://en.wikipedia.org/wiki/Deep_Blue_versus_Garry_Kasparov#Game_5_2
+        (gra 6, link pokazuje 5 ale w zadaniu jest ostatni stan niech bedzie gra 6)
+        napisze tylko czarne bo wiem o co chodzi
+         */
+        int pawn = 0;
+        int rook = 1;
+        int knight = 2;
+        int bishop = 3;
+        int queen = 4;
+        int king = 5;
+
+        int[][] board = new int [8][8];
+
+        board[0][0] = rook;
+        board[2][0] = king;
+        board[7][0] = rook;
+
+        board[0][1] = pawn;
+        board[3][1] = knight;
+        board[4][1] = bishop;
+        board[6][1] = pawn;
+
+        board[2][2] = bishop;
+        board[7][2] = pawn;
+
+        board[1][3] = pawn;
+        board[3][3] = knight;
+        board[5][3] = pawn;
+
     }
 
 }
